abb115793ee37b8ef402aaaef45d490d33ee682d
sahishnu1123 <102507316+sahishnu1123@users.noreply.github.com>
2022-04-20 13:44:40 +0530

    Update cnstrct_fig4.py

--- Assignment 1/codes/cnstrct_fig4.py
+++ Assignment 1/codes/cnstrct_fig4.py
@@ -22,6 +22,8 @@ plt.plot(4*cos(104), 4*sin(104), marker = ".", ms = 10, mfc = "green", mec = "gr
 
 plt.plot(4*cos(164), 4*sin(164), marker = ".", ms = 10, mfc = "green", mec = "green")
 
+plt.plot(4*cos(110), -4*sin(110), marker = ".", ms = 10, mfc = "green", mec = "green")
+
 x1 = [0,4]
 y1 = [0,0]
 plt.plot(x1,y1, color = "black")
@@ -34,9 +36,14 @@ x3 = [0,4*cos(164)]
 y3 = [0,4*sin(164)]
 plt.plot(x3,y3, color = "black")
 
+x4 = [0,4*cos(110)]
+y4 = [0,-4*sin(110)]
+plt.plot(x4,y4, color = "black")
+
 plt.annotate("P", (3.7,0.3))
 plt.annotate("Q", (-1.5,3.9))
 plt.annotate("R", (-4.1,1.25))
+plt.annotate("S", (-1.6,-4.1))
 plt.annotate("C", (0.2,0.2))
 plt.axis("scaled")
 
