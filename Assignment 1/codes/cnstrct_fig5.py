8d8ad21cb1fee64aa1a9b677c21194f2224c5fa8
sahishnu1123 <102507316+sahishnu1123@users.noreply.github.com>
2022-04-28 18:42:40 +0530

    Update cnstrct_fig5.py

--- Assignment 1/codes/cnstrct_fig5.py
+++ Assignment 1/codes/cnstrct_fig5.py
@@ -22,62 +22,68 @@ def sin(a):
     p = np.sin(a*np.pi/180)
     return p
 
-c = plt.Circle((0,0),radius = r, facecolor = "white", edgecolor = "Black")
+def u_vec(theta):
+    v = np.array([cos(theta),sin(theta)])
+    return v
+    
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+def plot(x):
+    plt.plot(x[0,:], x[1,:], 'k')
+    return 0
+
+C = np.array([0,0])
+P = r*u_vec(0)
+Q = r*u_vec(PCQ)
+R = r*u_vec(PCR)
+S = r*u_vec(360 - PCS)
+
+c = plt.Circle((C),radius = r, facecolor = "white", edgecolor = "Black")
 
 ax = plt.gca()
 ax.add_patch(c)
 
-plt.plot(0,0, marker = ".", ms = 10, mfc = "green", mec = "green")
+x_PC = line_gen(P,C)
+x_QC = line_gen(Q,C)
+x_RC = line_gen(R,C)
+x_SC = line_gen(S,C)
+x_PQ = line_gen(P,Q)
+x_QR = line_gen(Q,R)
+x_RS = line_gen(R,S)
+x_SP = line_gen(S,P)
 
-plt.plot(r,0, marker = ".", ms = 10, mfc = "green", mec = "green")
+plot(x_PC)
+plot(x_QC)
+plot(x_RC)
+plot(x_SC)
+plot(x_PQ)
+plot(x_QR)
+plot(x_RS)
+plot(x_SP)
 
-plt.plot(r*cos(PCQ), r*sin(PCQ), marker = ".", ms = 10, mfc = "green", mec = "green")
 
-plt.plot(r*cos(PCR), r*sin(PCR), marker = ".", ms = 10, mfc = "green", mec = "green")
-
-plt.plot(r*cos(PCS), -r*sin(PCS), marker = ".", ms = 10, mfc = "green", mec = "green")
-
-x1 = [0,r]
-y1 = [0,0]
-plt.plot(x1,y1, color = "black")
-
-x2 = [0,r*cos(PCQ)]
-y2 = [0,r*sin(PCQ)]
-plt.plot(x2,y2, color = "black")
-
-x3 = [0,r*cos(PCR)]
-y3 = [0,r*sin(PCR)]
-plt.plot(x3,y3, color = "black")
-
-x4 = [0,r*cos(PCS)]
-y4 = [0,-r*sin(PCS)]
-plt.plot(x4,y4, color = "black")
-
-x5 = [r*cos(PCQ),r*cos(PCR)]
-y5 = [r*sin(PCQ),r*sin(PCR)]
-plt.plot(x5,y5, color = "black")
-
-x6 = [r*cos(PCS),r*cos(PCR)]
-y6 = [-r*sin(PCS),r*sin(PCR)]
-plt.plot(x6,y6, color = "black")
-
-x7 = [r*cos(PCS),r]
-y7 = [-r*sin(PCS),0]
-plt.plot(x7,y7, color = "black")
-
-x8 = [r*cos(PCQ),r]
-y8 = [r*sin(PCQ),0]
-plt.plot(x8,y8, color = "black")
+coords = np.vstack((P,Q,R,S,C)).T
+plt.scatter(coords[0,:], coords[1,:])
+vert_labels = ['P','Q','R','S','C']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (coords[0,i], coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(-8,6), # distance from text to points (x,y)
+                 ha='left') # horizontal alignment can be left, right or center
 
 
 
-plt.annotate("P", (r + (r/20), r/20))
-plt.annotate("Q", ((r*cos(PCQ) + (r/40)), (r*sin(PCQ) + (r/20))))
-plt.annotate("R", ((r*cos(PCR) - (r/16)), (r*sin(PCR) + (r/20))))
-plt.annotate("S", ((r*cos(PCS) - (r/20)), (-r*sin(PCS) - (r/10))))
-plt.annotate("C", (r/25,r/25))
+
 plt.axis("scaled")
-
 plt.axis('off')
 
 plt.show()
